866a9560f17cfbb963bbed0d8149a432973d438c
Alfred Moore <ak_moore@live.com>
2018-02-11 00:52:23 -0600

    Initial Commit & Migrations

---
+++ database/migrations/2018_02_11_064042_create_runs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRunsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('runs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('track_id');
+            $table->boolean('is_competition');
+            $table->string('competition_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('runs');
+    }
+}
---
+++ database/migrations/2018_02_11_064126_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('run_id')->index();
+            $table->string('distance');
+            $table->integer('time');
+            $table->string('wind');
+            $table->integer('rating')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
