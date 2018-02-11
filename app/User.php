83a6cb701d09d76cab3128a01cab926b8da8e8a8
Alfred Moore <ak_moore@live.com>
2018-02-11 01:50:25 -0600

    Established Authorization & Added Paper-Kit Pro

--- app/User.php
+++ app/User.php
@@ -3,30 +3,39 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, Sluggable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'full_name'
+            ]
+        ];
+    }
+
     protected $fillable = [
         'first_name', 'last_name', 'email', 'password',
         'slug',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    protected $appends = [
+        'full_name'
+    ];
+
+    //Attributes
+    public function getFullNameAttribute(){
+        return "{$this->first_name} {$this->last_name}";
+    }
 
     //Relationships
     public function tracks(){
